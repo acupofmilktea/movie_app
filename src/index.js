3f8fab78855ce803a459c096e4dd2d38857efb6e
SEONGYEONG <mtoc227@gmail.com>
2019-09-29 21:27:40 +0900

    2019-09-29 Learn React: Props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 리액트는 하나의 컴포넌트만을 렌더링한다
+ReactDOM.render(<App />, document.getElementById("root"));
