799650ff7f1b5484369d0cc815bcb393cf3a9ad5
SEONGYEONG <mtoc227@gmail.com>
2019-10-10 22:17:46 +0900

    Updates

---
+++ precache-manifest.92650945d91a69b28b91ff7b9d3dda85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987b1e355ce9d94e3203d21e27601917",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3c0e2f6d07ac3c7021f5",
+    "url": "/movie_app/static/css/main.ddefd611.chunk.css"
+  },
+  {
+    "revision": "53c1595b8a1051453813",
+    "url": "/movie_app/static/js/2.e47595bb.chunk.js"
+  },
+  {
+    "revision": "3c0e2f6d07ac3c7021f5",
+    "url": "/movie_app/static/js/main.be783174.chunk.js"
+  },
+  {
+    "revision": "d72445ade3fc7a4cdf4d",
+    "url": "/movie_app/static/js/runtime-main.b4758674.js"
+  }
+]);
